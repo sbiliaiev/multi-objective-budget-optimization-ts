e448d92198d4705fb34b39b779c69a5739be2f42
Serhii Biliaiev <ocnskyy@gmail.com>
2018-06-18 02:35:41 +0300

    Add types

---
+++ src/types/index.ts
@@ -0,0 +1,19 @@
+export interface IAlternative {
+  number: number
+  cost: number
+  economic: number
+  ecologic: number
+  social: number
+}
+
+export type Data = IAlternative[]
+
+export interface IStrategy {
+  economic: number
+  ecologic: number
+  social: number
+}
+
+export interface INormilizedMatrix extends IStrategy {
+  summaryEfficiency: number
+}
