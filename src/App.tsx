3a8a3e8a58245419c79ae8a40732fe8bf239f2f3
Serhii Biliaiev <ocnskyy@gmail.com>
2018-06-18 02:36:37 +0300

    Add first layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import * as React from 'react';
-import './App.css';
+import * as React from 'react'
+import './App.css'
 
-import logo from './logo.svg';
+import logo from './logo.svg'
+
+// Components
+import { Button, ButtonToolbar, Table } from 'react-bootstrap'
+
+// Data
+import { defaultValues } from './data'
+// import {ecologicStrategy, economicStrategy, socialStrategy} from './data'
+
+// Utils
+// import { createNormilizeMatrix, sortNormilizedMatrix } from './utils'
+
+// Types
+import { IAlternative } from './types'
 
 class App extends React.Component {
   public render() {
@@ -9,14 +22,40 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">
+            Багатокритеріальна оптимізація списку альтернатив в залежності від стратегії розвитку регіону
+          </h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p className="App-intro">Початковий набір даних</p>
+        <Table striped bordered condensed hover>
+          <thead>
+            <tr>#</tr>
+            <tr>Вартість</tr>
+            <tr>Економічний критерій</tr>
+            <tr>Екологічній критерій</tr>
+            <tr>Соціальний критерій</tr>
+          </thead>
+          <tbody>
+            {defaultValues.map((alternative: IAlternative) => (
+              <tr key={alternative.number}>
+                <td>{alternative.number}</td>
+                <td>{alternative.cost}</td>
+                <td>{alternative.economic}</td>
+                <td>{alternative.ecologic}</td>
+                <td>{alternative.social}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+
+        <ButtonToolbar>
+          <Button bsStyle="primary">Розрахувати для економічної стратегії</Button>
+          <Button bsStyle="success">Розрахувати для екологічної стратегії</Button>
+          <Button bsStyle="warning">Розрахувати для соціальної стратегії</Button>
+        </ButtonToolbar>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
