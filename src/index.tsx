c06630772c974e9fba0b0acc8638a7dad9727a67
Serhii Biliaiev <ocnskyy@gmail.com>
2018-06-18 03:43:34 +0300

    Add bootstrap styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import App from './App'
+import './index.css'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
-);
-registerServiceWorker();
+const exact: boolean = true
+
+ReactDOM.render(<App />, document.getElementById('root') as HTMLElement)
+registerServiceWorker()
